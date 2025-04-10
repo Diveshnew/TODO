1b0be521492022169c150336098a580f5897d599
Divesh Dhek <workwithdivesh@gmail.com>
2025-04-10 23:15:52 +0530

    update logo and favicon

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <nav className="w-full fixed top-0 left-0 flex justify-between items-center bg-white px-8 py-3 z-50">
-      <Link className="text-black font-bold" href="/">
-        TODO
+      <Link
+        href="/"
+        className="flex items-center space-x-2 text-black font-bold"
+      >
+        <Image src="/logo.png" alt="Logo" width={30} height={30} />
+        <span>TODO</span>
       </Link>
     </nav>
   );
